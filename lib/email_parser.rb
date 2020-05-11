9150199160d3e2f2d241389eb32b1547536398f7
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-05-11 13:27:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,17 +3,3 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailParser 
-  attr_accessor :csv_email
-  
-  def initialize(csv_emailsls)
-    @csv_emails = cvs_emails
-  end
-  
-  def parse 
-    csv_emails.split.collect do |address| 
-      address.split(',')
-  end 
-  .flatten.uniq
-  end
-end
